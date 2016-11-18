a4c3f51ab8f906585b4d25d64989054bd1e558de
Nathan Mynarcik <nathan.mynarcik@match.com>
2016-11-18 09:59:48 -0600

    newrelic js file

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Slack Challonger'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'f8557dca7b3a647bee2e042ee170047d527b2a57',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
